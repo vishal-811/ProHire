5808109b07a0c663a2b3ae387a6f9c16b9dec094
vishalsharma <vishalssharma811@gmail.com>
2024-03-28 19:48:22 +0530

    fix port

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(
       tempFileDir: "/tmp/",
     })
   );
-app.listen(3000);
-// app.listen(process.env.PORT | 3000, ()=>{
-//     console.log(`Server is runnig on ${process.env.PORT}`)
-// });
+
+app.listen(process.env.PORT | 3000, ()=>{
+    console.log(`Server is runnig on ${process.env.PORT}`)
+});
